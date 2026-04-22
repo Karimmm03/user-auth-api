91685c9d4f01ec0b2ef9fe81c7794556760b0ba3
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-04-22 06:20:48 +0200

    fix: exclude refreshToken from user profile response

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ const User = require('../models/user.model');
 
 router.get('/me', protect, async (req, res) => {
     try{
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await User.findById(req.user.id).select('name email createdAt');
         res.status(200).json({user});
     } catch(error){
         res.status(500).json({message: 'Server error'});
