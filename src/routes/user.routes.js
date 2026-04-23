ab956159a48aea788108d19b4dd5f7ba7f7ad3e5
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-04-23 15:44:34 +0200

    refactor(routes): delegate unhandled user route errors to global handler

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const {protect} = require('../middlewares/auth.middleware');
 const User = require('../models/user.model');
 
-router.get('/me', protect, async (req, res) => {
+router.get('/me', protect, async (req, res, next) => {
     try{
         const user = await User.findById(req.user.id).select('name email createdAt');
         res.status(200).json({user});
     } catch(error){
-        res.status(500).json({message: 'Server error'});
+        next(error);
     }
 });
 
