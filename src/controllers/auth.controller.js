efc54fb60bec1fdd7663a7ff9a4ac962b32c4a71
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-04-27 01:02:18 +0300

    refactor(auth): destructure message in resendVerificationEmail controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -74,8 +74,8 @@ const verifyEmail = async (req, res, next) => {
 
 const resendVerificationEmail = async (req, res, next) => {
     try {
-        const data = await authService.resendVerificationEmail(req.body);
-        res.status(200).json(data);
+        const {message} = await authService.resendVerificationEmail(req.body);
+        res.status(200).json({message});
     } catch (error){
         next(error);
     }
