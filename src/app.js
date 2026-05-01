949cdf8745a2f3335bae4be9f5fe964d9db1f1af
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-05-01 03:36:19 +0300

    feat(security): configure CORS and HTTP security headers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const {errorHandler} = require('./middlewares/error.middleware');
 
 const app = express();
 
+app.use(helmet());
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
